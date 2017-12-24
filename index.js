13623f7d64159cc13e7de963167868eedd71b6e2
David Björklund <david.bjorklund@gmail.com>
2017-12-24 07:02:08 +0100

    use pirates instead of require-hacker

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 'use strict'
 
-const fs = require('fs')
 const { transform } = require('sucrase')
-const requireHacker = require('require-hacker')
+const { addHook } = require('pirates')
 
 module.exports = opts =>
-  requireHacker.hook('jsx', filename => {
-    const code = fs.readFileSync(filename, 'utf8')
-    const transformed = transform(code, opts)
-
-    return transformed
-  })
+  addHook(code => transform(code, opts), { exts: ['.jsx'] })
